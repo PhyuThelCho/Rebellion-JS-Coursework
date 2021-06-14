cd7ff6f6273a1a017c4f456297d648501de37a3d
PhyuThelCho <84267958+PhyuThelCho@users.noreply.github.com>
2021-06-14 17:18:11 +0630

    Create IfStatement.js
    IfStatement

---
+++ IfStatement.js
@@ -0,0 +1,17 @@
+let age= prompt ('How old are you?');
+
+if (age>= 13 && age<= 19){
+console.log('You're a teenager!');
+}
+else if (age>= 5 && age<= 12){
+console.log('You're a child!');
+}
+else if (age>= 2 && age<= 4){
+console.log('You're a toddler!');
+}
+else if (age>= 0 && age<= 1){
+console.log('You're an infant!');
+}
+else{
+console.log('You're an adult!');
+}
